c79c85276b2a6d9a2c293a8e80893a4e9f6b716a
LorenB <loren.barcus@gmail.com>
2018-05-04 08:30:19 -0400

    Create a test for a user loading an empty page.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,10 @@
 const {assert} = require('chai');
+
+describe('User visits landing page', () => {
+  describe('before any videos have been uplaoded', () => {
+    it('render the page without videos', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#videos-container'), '');
+    })
+  });
+});
