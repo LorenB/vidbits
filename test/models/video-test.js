0b2c176634749764f7e0bccac71d035b712c1d93
LorenB <loren.barcus@gmail.com>
2018-05-07 20:56:10 -0400

    Add model test. Ensure database changes isolated to a specific test.

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,3 +1,4 @@
+const Video = require('../../models/video');
 const {assert} = require('chai');
 const {mongoose, databaseUrl, options} = require('../../database');
 
@@ -9,6 +10,26 @@ async function connectDatabase() {
 async function disconnectDatabase() {
   await mongoose.disconnect();
 }
+
+describe('Model: Video', () => {
+  beforeEach( () => {
+    connectDatabase();
+  });
+
+  afterEach( () => {
+    disconnectDatabase();
+  });
+
+  describe('#title', () => {
+    it('is a string', async () => {
+      const titleInvalid = 0;
+      const video = new Video({
+        title: titleInvalid
+      });
+      assert.strictEqual(video.title, titleInvalid.toString() );
+    });
+  });
+});
 
 module.exports = {
   connectDatabase,
--- test/routes/create-video-test.js
+++ test/routes/create-video-test.js
@@ -1,9 +1,21 @@
 const {assert} = require('chai');
 const request = require('supertest');
 const app = require('../../app');
+const {mongoose, databaseUrl, options} = require('../../database');
+
+const Video = require('../../models/video');
+
 
 describe('Server path: /videos', () => {
   describe('POST', () => {
+    beforeEach(async () => {
+      await mongoose.connect(databaseUrl, options);
+      await mongoose.connection.db.dropDatabase();
+    });
+    afterEach(async () => {
+      await mongoose.disconnect();
+    });
+
     it('creates a video and persists it', async () => {
       const response = await request(app)
         .post('/videos')
@@ -11,6 +23,7 @@ describe('Server path: /videos', () => {
         .send({title: 'Some Video', desciption: 'A video about things and stuff.'});
       assert.equal(response.status, 201);
       // TODO: confirm data is actually persisted
+      const createdVideo = await Video.findOne({});
     });
   });
 });
