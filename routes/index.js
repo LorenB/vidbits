2cf1c974476b6e69645a8475b9cdddfc537c01ab
LorenB <loren.barcus@gmail.com>
2018-05-08 21:51:04 -0400

    Persist data in post request to database.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
+const Video = require('../models/video');
+
 router.post('/videos', async (req, res) => {
+  const {title, description} = req.body;
+  await Video.create({title, description});
   res
     .status(201)
     .send();
