11dbad90db04f7d39f6640a7754c51c3c9ec33b4
LorenB <loren.barcus@gmail.com>
2018-05-08 23:01:02 -0400

    Replace public/index.html with a handlebars template.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const Video = require('../models/video');
 
+router.get('/', async (req, res) => {
+  res.render('index');
+});
+
 router.post('/videos', async (req, res) => {
   const {title, description} = req.body;
   await Video.create({title, description});
