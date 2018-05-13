cf7fd6062a85a81d83114c6a1e3dfe39dba45ee6
LorenB <loren.barcus@gmail.com>
2018-05-13 16:47:38 -0400

    An iframe exists with the correct src for an exiting video.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -3,6 +3,10 @@ const {assert} = require('chai');
 const {connectDatabase, disconnectDatabase} = require('../database-utilities');
 const Video = require('../../models/video');
 
+const generateRandomUrl = (domain) => {
+  return `http://${domain}/${Math.random()}`;
+};
+
 describe('User visits landing page', () => {
   describe('before any videos have been uplaoded', () => {
     it('render the page without videos', () => {
@@ -21,20 +25,14 @@ describe('User visits landing page', () => {
     it('should show a video uploaded in an iframe', async () => {
       const title = 'Some title';
       const description = 'Some description';
-      const url = 'http://example.com';
+      const url = generateRandomUrl('example.com');
       const video = await Video.create({title, description, url});
       browser.url('/videos');
       const videosContainer = await browser.getText('#videos-container');
-
-
       assert.notEqual(videosContainer, '');
       assert.include(videosContainer, title);
-      // //TODO: determine if retrieving an iframes src is allowed
-      // const videoIframeSrc = await browser.getAttribute('.video-card iframe', 'src');
-      // assert.equal(videoIframeSrc, videoUrl);
-      // assert.include(videosContainer.toString(), videoUrl);
-
+      const titleLink = browser.getText(`iframe[src="${url}"]`);
+      assert.ok(titleLink !== null);
     });
   });
-
 });
