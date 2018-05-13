b6c849cc54d4235dc7371bfd0d02ac9fccb54ec6
LorenB <loren.barcus@gmail.com>
2018-05-13 16:36:11 -0400

    Add title verification to feature test.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -21,13 +21,14 @@ describe('User visits landing page', () => {
     it('should show a video uploaded in an iframe', async () => {
       const title = 'Some title';
       const description = 'Some description';
-      const videoUrl = 'http://example.com';
-      const video = await Video.create({title, description, videoUrl});
+      const url = 'http://example.com';
+      const video = await Video.create({title, description, url});
       browser.url('/videos');
       const videosContainer = await browser.getText('#videos-container');
-      console.log('videosContainer', videosContainer);
-      //video-card
+
+
       assert.notEqual(videosContainer, '');
+      assert.include(videosContainer, title);
       // //TODO: determine if retrieving an iframes src is allowed
       // const videoIframeSrc = await browser.getAttribute('.video-card iframe', 'src');
       // assert.equal(videoIframeSrc, videoUrl);
