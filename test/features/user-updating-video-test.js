490db5cb771036b1f5ea0c1de2c35de59de0c0b2
LorenB <loren.barcus@gmail.com>
2018-05-16 09:00:56 -0400

    Ensure browser enters data before submitting in edit flow. Check title is updated.

--- test/features/user-updating-video-test.js
+++ test/features/user-updating-video-test.js
@@ -32,10 +32,24 @@ describe('User creates video', () => {
       browser.setValue('#description-input', videoDescription);
       browser.setValue('#url-input', videoUrl);
       await browser.click('#submit-button');
+
       await browser.click('#edit');
+      const browserUrlForEdit = await browser.getUrl();
+      const browserUrlForEditParts = browserUrlForEdit.split('/');
+      const vidoeId = browserUrlForEditParts[4];
+
       const videoTitleUpdated = 'A truly medicore video.';
-      browser.setValue('#title-input', videoTitleUpdated);
+      await browser.setValue('#title-input', videoTitleUpdated);
+      await browser.setValue('#description-input', videoDescription);
+      await browser.setValue('#url-input', videoUrl);
+
       await browser.click('#submit-button');
+
+      const browserUrlAfterEdit = await browser.getUrl();
+      assert.include(browserUrlAfterEdit, vidoeId);
+      const titleElem = await browser.getText('.single-video-title');
+      assert.include(titleElem, videoTitleUpdated);
+
       const videos = await Video.find();
       assert.equal(videos.length, 1);
     });
