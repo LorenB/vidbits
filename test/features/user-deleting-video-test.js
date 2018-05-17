da6d6608a8dc9572ccdbe14b1575f3fede8e9873
LorenB <loren.barcus@gmail.com>
2018-05-16 20:25:33 -0400

    Test (feature): User can delete video

---
+++ test/features/user-deleting-video-test.js
@@ -0,0 +1,23 @@
+const {assert} = require('chai');
+
+describe('User deletes video', () => {
+  describe('clicks delete', () => {
+    it('removes the video from the list', async () => {
+      browser.url('/videos/create');
+      const randomNum = Math.random();
+      const videoTitle = `Random video number ${randomNum}`;
+      const videoDescription = `Pretty random ${randomNum}`;
+      const videoUrl = `http://example.com/${randomNum}`;
+      await browser.setValue('#title-input', videoTitle);
+      await browser.setValue('#description-input', videoDescription);
+      await browser.setValue('#url-input', videoUrl);
+      await browser.click('#submit-button');
+      await browser.click('#delete');
+      await browser.url('/videos');
+      const body = await browser.getText('body');
+      assert.notInclude(body, videoTitle);
+      assert.notInclude(body, videoDescription);
+      assert.notInclude(body, videoUrl);
+    });
+  });
+});
